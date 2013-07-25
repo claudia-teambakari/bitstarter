472b425af1beee714c9c0d9430c64dfd0e343c2e
Claudia Blossom <claudia@teambakari.com>
2013-07-25 21:55:36 +0000

    Node dependency express installed

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
-//var express = require('express');
-//var app = express();
-//app.use(express.logger());
+ 
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
 app.get('/', function(request, response) {
 
 var fs = require('fs');
-var file = fs.readFileSync(' index.html', 'utf-8');
+var file = fs.readFileSync(' index.html', 'utf8');
   response.send(file);
   
 //app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 });
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+  
