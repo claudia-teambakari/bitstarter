f87ec0d75edc3c3175550cf201a68825997e115d
Claudia Blossom <claudia@teambakari.com>
2013-07-25 21:59:23 +0000

    Node dependency express installed

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
 
 var fs = require('fs');
-var file = fs.readFileSync(' index.html', 'utf8');
+var file = fs.readFileSync('index.html', 'utf8');
   response.send(file);
   
 //app.get('/', function(request, response) {
