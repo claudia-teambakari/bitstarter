12356a2d5036330c137e2209671b775d301e7983
Claudia Blossom <claudia@teambakari.com>
2013-07-25 20:43:04 +0000

    Test web.js app

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var x = fs.readFileSync('index.html', 'utf8');
+  response.send(x);
+  
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
